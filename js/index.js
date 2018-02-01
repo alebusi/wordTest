36f026b8e58ed280830955c4f1b34d24f939a111
alebusi <allebusi@gmail.com>
2018-02-01 15:03:54 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 var colorCouples = [["#fff", "#000"]];
-var fontType = ['Roboto', 'Roboto Mono', 'PT Sans Caption'];
+var fontType = ['Roboto', 'PT Sans Caption'];
 
 var containerSpan = document.getElementById("container"),
   firstSpan = document.getElementById("letterOne"),
