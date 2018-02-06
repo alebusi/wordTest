a305818ad1eafa0411e0e073b3244baa96b9cfb5
alebusi <allebusi@gmail.com>
2018-02-06 15:07:14 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 //var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+var lettersC = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+var lettersV = ["a","e","i","o","u"];
+var lettersVn = ["a","e","i","o"];
+		 
 var colorCouples = [["#fff", "#000"]];
 var fontType = ['Roboto', 'PT Sans Caption'];
 
@@ -14,8 +16,14 @@ function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function letterSet (spanName, colorValue, numberValue, fontValue) {
-	spanName.innerHTML = letters[(randomIntFromInterval(0,letters.length - 1))];
+function letterSetC (spanName, colorValue, numberValue, fontValue) {
+	spanName.innerHTML = lettersC[(randomIntFromInterval(0,letters.length - 1))];
+	spanName.style.color = colorCouples[colorValue][numberValue];
+	spanName.style.fontFamily = fontType[fontValue];
+}
+
+function letterSetV (spanName, colorValue, numberValue, fontValue) {
+	spanName.innerHTML = lettersV[(randomIntFromInterval(0,letters.length - 1))];
 	spanName.style.color = colorCouples[colorValue][numberValue];
 	spanName.style.fontFamily = fontType[fontValue];
 }
@@ -28,6 +36,7 @@ function randomMaker() {
 	resetSpan(firstSpan);
 	resetSpan(secondSpan);
 	resetSpan(thirdSpan);
+	resetSpan(fourSpan);
 
 // Picks either 1 or 0 to get the first or second color from the array
 var eitherOneOrZero = 0, //randomIntFromInterval(0,1),
@@ -42,13 +51,12 @@ var colorPairing = randomIntFromInterval(0,colorCouples.length - 1),
 // Picks a color for the background
 	containerSpan.style.backgroundColor = colorCouples[colorPairing][eitherOneOrZero];
 
-	letterSet(firstSpan, colorPairing, theOtherValue, fontNumber);
-	letterSet(secondSpan, colorPairing, theOtherValue, fontNumber);
+	letterSetC(firstSpan, colorPairing, theOtherValue, fontNumber);
+	letterSetV(secondSpan, colorPairing, theOtherValue, fontNumber);
+        letterSetC(thirdSpan, colorPairing, theOtherValue, fontNumber);
+        letterSetV(fourSpan, colorPairing, theOtherValue, fontNumber);
 
-	numletters == 0 ? letterSet(thirdSpan, colorPairing, theOtherValue, fontNumber) : null;
-
-	// letterSet(thirdSpan, colorPairing, theOtherValue, fontNumber);
-	
+	//numletters == 0 ? letterSet(thirdSpan, colorPairing, theOtherValue, fontNumber) : null;	
 }
 
 randomMaker();
